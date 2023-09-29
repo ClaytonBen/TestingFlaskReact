54d8c9df543d96d3587a6497b7f62efbf7273a4c
Ben Clayton <benjoclayton@gmail.com>
2023-09-28 21:30:27 -0400

    updating player info

--- src/backend/test.py
+++ src/backend/test.py
@@ -10,24 +10,23 @@ id_to_name = {owner['owner_id']: owner['display_name'] for owner in owners}
 
 player_lookup = api_functions.ApiFunctions.get_player_info('players.json')
 
-print(rosters)
-rosters = []
+full_rosters = []
 for owner in rosters['lineups']:
     starters = []
     for starter in owner['starters']:
-        if starter != "0":
-            player_lookup[starter]
+        if starter != '0':
+            starters.append(player_lookup[starter])
 
     players = []
     for player in owner['players']:
         if starter != "0":
-            player_lookup[starter]
+            players.append(player_lookup[starter])
     results ={
         'name': id_to_name[owner['owner_id']],
         'starters': starters,
         'players': players
     }
-    rosters.append(results)
+    full_rosters.append(results)
 
 with open("sample.json", "w") as outfile:
-    json.dump(players, rosters)
+    json_string = json.dump(full_rosters, outfile)
