225f8831aa591e5d5b59cb37100e3158d8f93187
Ben Clayton <benjoclayton@gmail.com>
2023-09-29 09:00:48 -0400

    getting the lineups setup

--- src/backend/test.py
+++ src/backend/test.py
@@ -3,8 +3,8 @@ import json
 from api_functions import api_functions
 
 
-rosters = api_functions.ApiFunctions.get_rosters("695699151924555776")
-owners = api_functions.ApiFunctions.get_owner_info("695699151924555776")
+rosters = api_functions.ApiFunctions.get_rosters("930602680773357568")
+owners = api_functions.ApiFunctions.get_owner_info("930602680773357568")
 
 id_to_name = {owner['owner_id']: owner['display_name'] for owner in owners}
 
@@ -15,12 +15,19 @@ for owner in rosters['lineups']:
     starters = []
     for starter in owner['starters']:
         if starter != '0':
-            starters.append(player_lookup[starter])
-
+            starter_info = player_lookup[starter]
+            if starter_info['position'] == "DEF":
+                starters.append(starter_info["team"])
+            else:
+                starters.append(starter_info['full_name'])
     players = []
     for player in owner['players']:
-        if starter != "0":
-            players.append(player_lookup[starter])
+        if player != '0':
+            player_info = player_lookup[player]
+            if player_info['position'] == "DEF":
+                players.append(player_info["team"])
+            else:
+                players.append(player_info['full_name'])
     results ={
         'name': id_to_name[owner['owner_id']],
         'starters': starters,
@@ -28,5 +35,5 @@ for owner in rosters['lineups']:
     }
     full_rosters.append(results)
 
-with open("sample.json", "w") as outfile:
+with open("lineups.json", "w") as outfile:
     json_string = json.dump(full_rosters, outfile)
